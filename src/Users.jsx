b1b8d5984296d6780be344d712711f02682a4052
akshay5568 <adityajangid124@gmail.com>
2025-05-25 13:21:38 +0530

    Initial commit

--- src/Users.jsx
+++ src/Users.jsx
@@ -4,15 +4,10 @@ import React, { useState } from "react";
 
 function Users(){
 
-   const [score,setScore] = useState({name : "Aditya", age: 20,  desgnation: "Full Stack Developer" , isEmployeed: true  });
-   let a = false;
+   const [score,setScore] = useState([1,2,3,4,5,6,7]);
     return (
        <div>
-         <h1>Name: {score.name}</h1>
-         <h1>Age : {score.age}</h1>
-        <h1>Designation : {score.desgnation}</h1>
-        <h1>isEmployeed : {score.isEmployeed.toString()}</h1>
-        <button onClick={() => setScore({...score , isEmployeed: score.isEmployeed ? a : true})}>{score.isEmployeed ? "Fire Now" : "Join Now"}</button>
+          {score.map(item => <h1>{item}</h1>)}
     </div>
     )
 }
